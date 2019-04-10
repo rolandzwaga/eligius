213dc11e577e933d3b56dc7bc743df9f85f83e29
Roland Zwaga <rbzwaga@gmail.com>
2019-04-10 22:30:03 +0200

    more eventbus unit tests

--- test/eventbus/eventbus.spec.js
+++ test/eventbus/eventbus.spec.js
@@ -79,6 +79,7 @@ describe('Eventbus', ()=> {
         const interceptor = {
             intercept: (args) => {
                 called += args[0];
+                return args;
             }
         };
         eventbus.registerInterceptor('test', interceptor, topic);
@@ -91,4 +92,58 @@ describe('Eventbus', ()=> {
         expect(called).to.be.equal(1);
     });
 
+    it('should register and call the event interceptor and pass the new args to the event handler', () => {
+        // given
+        let called1 = 0;
+        let called2 = 0;
+        let topic = 'topic';
+        const interceptor = {
+            intercept: (args) => {
+                return [10];
+            }
+        };
+        eventbus.registerInterceptor('test', interceptor);
+        eventbus.on('test', (val) => {
+            called1 += val;
+        });
+        eventbus.on('test', (val) => {
+            called2 += val;
+        }, topic);
+
+        // test
+        eventbus.broadcast('test', [1]);
+        eventbus.broadcastForTopic('test', topic, [1]);
+
+        // expect
+        expect(called1).to.be.equal(10);
+        expect(called2).to.be.equal(1);
+    });
+
+
+    it('should register and call the specified eventlistener', () => {
+        // given
+        let received = [];
+        const topic = 'topic';
+        const listener = {
+            handleEvent: (eventName, eventTopic, args) => {
+                received.push([eventName, eventTopic, args]);
+            }
+        };
+        eventbus.registerEventlistener(listener);
+
+        // test
+        eventbus.broadcast('test', [1]);
+        eventbus.broadcastForTopic('test2', topic, [100]);
+
+        // expect
+        expect(received.length).to.be.equal(2);
+
+        expect(received[0][0]).to.be.equal('test');
+        expect(received[0][1]).to.be.equal(null);
+        expect(received[0][2][0]).to.be.equal(1);
+
+        expect(received[1][0]).to.be.equal('test2');
+        expect(received[1][1]).to.be.equal(topic);
+        expect(received[1][2][0]).to.be.equal(100);
+    });
 });
