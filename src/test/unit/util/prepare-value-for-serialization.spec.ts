ad236dc97fe96b12602e117f206209db2108e305
rolandzwaga <scab@dds.nl>
2022-10-26 15:11:10 +0200

    Add unit tests for prepare-value-for-serialization

---
+++ src/test/unit/util/prepare-value-for-serialization.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import $ from 'jquery';
+import { suite } from 'uvu';
+import { prepareValueForSerialization } from '../../../util/prepare-value-for-serialization';
+
+const PrepareValueForSerializationSuite = suite('prepareValueForSerialization');
+
+PrepareValueForSerializationSuite('should seriaize the given object', () => {
+  // given
+  const object = {
+    a: 1,
+    b: $('body'),
+    c: function (i: number) {
+      return i;
+    },
+    d: [1, 'a', $('body'), (i: number) => i],
+  };
+
+  // test
+  const result = prepareValueForSerialization(object);
+
+  console.log('result', result);
+
+  // expect
+  expect(result.a).to.equal(1);
+  expect(result.b).to.equal('jQuery object');
+  expect(result.c).to.equal('function (i) {');
+  expect(result.d).to.eql([1, 'a', 'jQuery object', '(i) => i']);
+});
+
+PrepareValueForSerializationSuite.run();
--- src/util/prepare-value-for-serialization.ts
+++ src/util/prepare-value-for-serialization.ts
@@ -1,3 +1,5 @@
+import jQuery from 'jquery';
+
 export function prepareValueForSerialization(value: any): any {
   if (Array.isArray(value)) {
     return (value as Array<any>).map(prepareValueForSerialization);
@@ -5,14 +7,18 @@ export function prepareValueForSerialization(value: any): any {
     return 'jQuery object';
   } else if (typeof value === 'object') {
     return Object.fromEntries(
-      Object.entries(value).map(([name, value]) => [
-        name,
-        prepareValueForSerialization(value),
+      Object.entries(value).map(([propName, propValue]) => [
+        propName,
+        prepareValueForSerialization(propValue),
       ])
     );
   } else if (typeof value === 'function') {
     const funcString: string = value.toString();
-    return funcString.substring(0, funcString.indexOf('{') + 1);
+    const paranIndex = funcString.indexOf('{');
+    if (paranIndex > -1) {
+      return funcString.substring(0, funcString.indexOf('{') + 1);
+    }
+    return funcString;
   }
   return value;
 }
