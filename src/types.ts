07cf9a3b52bd179a8584a4d88752f54faf4caca4
rolandzwaga <scab@dds.nl>
2022-10-29 11:32:26 +0200

    Add code comments

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,22 @@
 import { IAction } from './action/types';
 import {
   IEngineConfiguration,
-  IResolvedEngineConfiguration,
+  IResolvedEngineConfiguration
 } from './configuration/types';
 import { IEventbus, IEventbusListener } from './eventbus/types';
 import { ITimelineProvider } from './timelineproviders/types';
 
+/**
+ * Describes an object that is capable of processing the given configuration and constructing an IEligiusEngine
+ * based on this configuration.
+ */
 export interface IEngineFactory {
+  /**
+   * Returns a fully configured IEligiusEngine instance.
+   * 
+   * @param engineConfig The given configuration
+   * @param resolver An optional resolver to process the given IEngineConfiguration. When not provided the IEngineFactory is expected to create their own instance.
+   */
   createEngine(
     engineConfig: IEngineConfiguration,
     resolver?: IConfigurationResolver
@@ -18,15 +28,31 @@ export interface IEngineFactoryOptions {
   devtools?: boolean;
 }
 
+/**
+ * Describes an object that is play one or more timelines, defined by a given set of ITimelineProviders.
+ */
 export interface IEligiusEngine {
+  /**
+   * Do the preparations needed in order to run the given timelines.
+   */
   init(): Promise<ITimelineProvider>;
+  /**
+   * Do the necessary cleanup before discarding this instance.
+   */
   destroy(): Promise<void>;
 }
 
+/**
+ * Describes an object that acts as a registry for imported resources.
+ */
 export interface ISimpleResourceImporter {
   import(name: string): Record<string, any>;
 }
 
+/**
+ * Describes an object that acts as a registry for imported resources and
+ * is able to return all of the registered operation, controller and provider names.
+ */
 export interface IResourceImporter extends ISimpleResourceImporter {
   getOperationNames(): string[];
   getControllerNames(): string[];
@@ -42,18 +68,27 @@ export interface IConfigurationResolver {
 
 export type TResultCallback = (result: any) => void;
 
+/**
+ * Describes a duration of time expressed in a start and an optional end range with a one second interval.
+ */
 export interface IDuration {
   start: number;
   end?: number;
 }
 
-export interface IStrictDuration {
-  start: number;
-  end: number;
-}
+/**
+ * Describes a duration of time expressed in a start and an end range with a one second interval.
+ */
+export interface IStrictDuration extends Required<IDuration> {}
 
+/**
+ * Describes the different kinds of timeline providers.
+ */
 export type TimelineTypes = 'animation' | 'mediaplayer';
 
+/**
+ * 
+ */
 export interface ITimelineProviderInfo {
   id: string;
   vendor: string;
