0d5af7f6a75600c000f480e255c1cea7fda56c23
rolandzwaga <scab@dds.nl>
2022-10-29 18:08:59 +0200

    Fix all of the uuid patterns

--- src/schema-tools/generate-operation-schemas.ts
+++ src/schema-tools/generate-operation-schemas.ts
@@ -76,7 +76,7 @@ function getTemplate() {
         title: 'The Id Schema',
         examples: ['123e4567-e89b-12d3-a456-426614174000'],
         pattern:
-          '^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$',
+          '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$',
       },
       systemName: {
         $id: '#/properties/systemName',
