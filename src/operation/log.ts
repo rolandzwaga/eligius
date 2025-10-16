2eab368c8ddbb78ce4cc6a2c60a9c669ffcd3bfa
Roland Zwaga <rbzwaga@gmail.com>
2025-10-16 22:25:16 +0200

    add logValue to log operation

--- src/operation/log.ts
+++ src/operation/log.ts
@@ -1,18 +1,30 @@
 import {getGlobals} from './helper/globals.ts';
-import type {TOperation, TOperationData} from './types.ts';
+import type {TOperation} from './types.ts';
+
+
+export interface ILogOperationData {
+  logValue?: any;
+}
 
 /**
- * This operation logs the current operation data, global data and context to the console.
+ * This operation logs the specified value, or, when no logValue property has been assigned
+ * it will log the current operation data, global data and context to the console
  */
-export const log: TOperation<TOperationData> = function (
-  operationData: TOperationData
+export const log: TOperation<ILogOperationData> = function (
+  operationData: ILogOperationData
 ) {
-  const globalData = getGlobals();
+  if ('logValue' in operationData) {
+    console.log(operationData.logValue);
+    delete operationData.logValue;
+  } else {
+    const globalData = getGlobals();
 
-  console.group('Operation info');
-  console.log('context', this);
-  console.log('operationData', operationData);
-  console.log('globalData', globalData);
-  console.groupEnd();
+    console.group('Operation info');
+    console.log('context', this);
+    console.log('operationData', operationData);
+    console.log('globalData', globalData);
+    console.groupEnd();
+  }
+  
   return operationData;
 };
